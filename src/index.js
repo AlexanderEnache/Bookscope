0bb295ce15436ecaadb2e7ef1b35dc2c142cb356
Alex Enache <ragnarxon@gmail.com>
2021-04-20 12:48:08 -0400

    Booklist Array

--- src/index.js
+++ src/index.js
@@ -1,17 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+let Book = ({title, image, author}) => {
+  return (
+    // <h1>jfed{console.log(props.props.title)}</h1>
+    <div className={"Book"}>
+      <h1 className={"Title"}>{title}</h1>
+      <img className={"Image"} src={image}/>
+      <p className={"Author"}>{author}</p>
+    </div>
+  );
+}
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <div className={"BookList"}>
+      <Book title="Stuff" image="https://images-na.ssl-images-amazon.com/images/I/51Cj8es7jmL._SX329_BO1,204,203,200_.jpg" author="Stuff"/>
+      <Book title="Stuff" image="https://images-na.ssl-images-amazon.com/images/I/51Cj8es7jmL._SX329_BO1,204,203,200_.jpg" author="Stuff"/>
+      <Book title="Stuff" image="https://images-na.ssl-images-amazon.com/images/I/51Cj8es7jmL._SX329_BO1,204,203,200_.jpg" author="Stuff"/>
+      <Book title="Stuff" image="https://images-na.ssl-images-amazon.com/images/I/51Cj8es7jmL._SX329_BO1,204,203,200_.jpg" author="Stuff"/>
+      <Book title="Stuff" image="https://images-na.ssl-images-amazon.com/images/I/51Cj8es7jmL._SX329_BO1,204,203,200_.jpg" author="Stuff"/>
+      <Book title="Stuff" image="https://images-na.ssl-images-amazon.com/images/I/51Cj8es7jmL._SX329_BO1,204,203,200_.jpg" author="Stuff"/>
+      <Book title="Stuff" image="https://images-na.ssl-images-amazon.com/images/I/51Cj8es7jmL._SX329_BO1,204,203,200_.jpg" author="Stuff"/>
+      <Book title="Stuff" image="https://images-na.ssl-images-amazon.com/images/I/51Cj8es7jmL._SX329_BO1,204,203,200_.jpg" author="Stuff"/>
+      <Book title="Stuff" image="https://images-na.ssl-images-amazon.com/images/I/51Cj8es7jmL._SX329_BO1,204,203,200_.jpg" author="Stuff"/>
+    </div>
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
